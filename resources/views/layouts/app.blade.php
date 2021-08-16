7cd077e4629f18b0286fc441a3b56da98e6084c8
Gison Jose Maran <gilson.maran@gmail.com>
2021-08-16 10:21:49 -0300

    Adicao do link Notificacao na barra de menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,6 +53,10 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="{{ route('notificacoes') }}">
+                                    Notificações
+                                </a>
+                                
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
