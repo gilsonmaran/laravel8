387b08ed92b788f88c796c52415ed8f7d0a1bec0
Gison Jose Maran <gilson.maran@gmail.com>
2021-08-16 23:19:30 -0300

    Funcionalidade de edicao de notificacao

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,21 +4,46 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\NotificationRequest;
 use App\Models\Notification;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class NotificationController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('notification.index');
     }
 
-    public function create() {
+    public function create()
+    {
         return view('notification.create');
     }
 
-    public function store(NotificationRequest $notification) {
+    public function store(NotificationRequest $notification)
+    {
         $store = $notification->except('_token');
-        $stored = Notification::create($store);
+        Notification::create($store);
+        return redirect()->route('notification');
+    }
+
+    public function edit($id)
+    {
+        $notification = DB::table('notifications')->find($id);
+
+        if (!$notification)
+            return view('notification.index');
+
+
+        return view('notification.edit', compact(['notification']));
+    }
+
+    public function update(NotificationRequest $notification, $id)
+    {
+        // dd($notification->all());
+        $update = $notification->except('_token');
+        Notification::find($id)->update($update);
+
         return redirect()->route('notification');
     }
 }
